7642371ca4b066db0d3b953437009b521c413798
Kyle Luzny <kluzny@gmail.com>
2015-04-17 17:52:53 -0500

    updated the link_script to use the correct paths and take backups

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 for dertferl in zshrc vimrc ackrc aliases gitignore; do
-  ln -sv $HOME/.$dertferl $HOME/.$dertferl
+  mv -vf $HOME/.$dertferl{,.$(date +"%Y%m%d-%H%M%S").bak}
+  ln -sv $HOME/dotfiles/.$dertferl $HOME/.$dertferl
 done
